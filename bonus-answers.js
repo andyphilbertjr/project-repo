53589068587543dfe29ec49c00c46cade28c33c3
andyphilbertjr <andyphilbertjr@gmail.com>
2018-09-19 22:09:58 -0400

    finished challenges

---
+++ bonus-answers.js
@@ -0,0 +1,48 @@
+//Bonus Code Challenges 
+
+//sort Years Problem 8
+var yearsArray = [ 1899, 2012, 1960, 1954, 2018 ]
+
+const sortYears = (yrArray) => yrArray.sort(( a, b ) => a - b)
+sortYears(yearsArray).reverse()
+
+//console.log(yearsArray)
+
+//End of Problem 8
+
+//just cool stuff Problem 9
+let arr1 = ['this', 'not this', 'nor this']
+let arr2 = ['thing 1', 'thing 2', 'this']
+
+function justCoolStuff(array1, array2){
+  return array1.filter( item => array2.includes(item) )
+}
+//console.log(justCoolStuff(arr1, arr2))
+
+//End of Problem 9
+
+// is the dinner vegan Problem 10
+const meals = [
+              {name: 'arugula', source: 'plant'},
+              {name: 'tomatoes', source: 'plant'},
+              {name: 'lemon', source:'plant'}, 
+              {name: 'olive oil', source: 'plant'}
+            ]
+const isTheDinnerVegan = (mealArray) => {
+  return mealArray.every( meal => meal.source === 'plant')
+}
+//console.log(isTheDinnerVegan(meals))
+
+// End of Problem 10 
+
+//dogFactory Problem 11 
+const dogFactory = (name = '', breed = '', weight = Number) => {
+  return {
+          name: name, 
+          breed: breed,
+          weight: weight
+        }
+}
+console.log(dogFactory('Joe', 'Pug', 27))
+
+//end of problem 11 
--- challenge-answers.js
+++ challenge-answers.js
@@ -20,7 +20,7 @@ function greetAliens(aliensArray){
        console.log(`Oh powerful ${alien}, we humans offer our unconditional surrender!\n`)
     }
 }
-//greetAliens(aliens) --Call
+//greetAliens(aliens) 
 
 //End of Greet Aliens Problem 2 Answer
 
@@ -33,7 +33,56 @@ function convertToBaby(animalsArray){
     }
     return babyAnimals
 }
-convertToBaby(animals)
+//console.log(convertToBaby(animals))
+
 //End of convertToBaby Problem 3 Answer
 
-//
+// Fix the broken code Problem 4
+const numbers = [5, 3, 9, 30];
+
+ const smallestPower = (numArray, i = 1) => {
+   return numArray.map((number) => {
+        while(i < number){ i = i * 2};
+        return i
+     })
+ }
+
+ //console.log(smallestPower(numbers)) 
+ 
+ //End fo Problem 4 
+
+ //Decline and Except Everything Problem 5 Part 1
+ const veggies = ['broccoli', 'spinach', 'cauliflower', 'broccoflower'];
+
+ const politelyDecline = (veg) => {
+       console.log('No ' + veg + ' please. I will have pizza with extra cheese.');
+ }
+ const declineEverything = (vegArray) => vegArray.forEach(veggie => politelyDecline(veggie))
+ //declineEverything(veggies)
+
+ //End of Part 1 
+ 
+ //Accepts everything Problem 5 Part 2
+ const acceptsEverything = (vegArray) => vegArray
+ .map(veg => console.log(`Ok, I guess I will eat some ${veg}.`))
+// acceptsEverything(veggies)
+
+//End of Part 2
+
+// square Nums problem 6
+const numArray = [2, 7, 9, 171, 52, 33, 14]
+
+const squareNums = numbers => numbers.map( number => number * number)
+//console.log(squareNums(numArray))
+
+//End of Problem 6
+
+//shout Greetings Problem 7
+const greetings = ['hello', 'hi', 'heya', 'oi', 'hey', 'yo'];
+const shoutGreetings = (greetingsArray) => greetingsArray
+.map( greetingUpper => greetingUpper.toUpperCase() )
+//console.log(shoutGreetings(greetings))
+
+//End of Problem 7 
+
+
